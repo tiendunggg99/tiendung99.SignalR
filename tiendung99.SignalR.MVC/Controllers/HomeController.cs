31631daf8d72321ed5876d3b5eba49da2a0fdd41
tiendunggg99 <tiendung_ms@outlook.com>
2022-08-29 23:14:21 +0700

    Add project files.

---
+++ tiendung99.SignalR.MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using tiendung99.SignalR.MVC.Models;
+
+namespace tiendung99.SignalR.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ tiendung99.SignalR.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tiendung99.SignalR.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
