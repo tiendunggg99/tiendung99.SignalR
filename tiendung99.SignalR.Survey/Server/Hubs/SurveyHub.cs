114453be8ff3ddcf8d7feb00cfc545bcc11e11a8
dane.dung <dane.dung@itlvn.com>
2022-09-05 09:36:53 +0700

    add Hub

---
+++ tiendung99.SignalR.Survey/Server/Hubs/SurveyHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Text.RegularExpressions;
+using tiendung99.SignalR.Survey.Shared;
+
+namespace tiendung99.SignalR.Survey.Server.Hubs
+{
+    public interface ISurveyHub
+    {
+        Task SurveyAdded(SurveySummary survey);
+        Task SurveyUpdated(Surveyy survey);
+    }
+
+    public class SurveyHub : Hub<ISurveyHub>
+    {
+        // No need to implement here the methods defined by ISurveyHub, their purpose is simply
+        // to provide a strongly typed interface.
+        // Users of IHubContext still have to decide to whom should the events be sent
+        // as in: await this.hubContext.Clients.All.SendSurveyUpdated(survey);
+
+        // These 2 methods will be called from the client
+        public async Task JoinSurveyGroup(Guid surveyId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, surveyId.ToString());
+        }
+        public async Task LeaveSurveyGroup(Guid surveyId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, surveyId.ToString());
+        }
+    }
+}
